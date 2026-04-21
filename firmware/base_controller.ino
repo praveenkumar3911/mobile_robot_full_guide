49bfa15c829c5f746452147180174b6f7efd5384
praveenkumar3911 <154548405+praveenkumar3911@users.noreply.github.com>
2026-04-21 16:54:09 +0530

    Add base controller for motor control in firmware

---
+++ firmware/base_controller.ino
@@ -0,0 +1,43 @@
+#include <ros.h>
+#include "diffbot_base_config.h"
+#include "base_controller.h"
+#include "adafruit_feather_wing/adafruit_feather_wing.h"
+
+ros::NodeHandle nh;
+
+using namespace diffbot;
+
+AdafruitMotorController motor_controller_right = AdafruitMotorController(MOTOR_RIGHT);
+AdafruitMotorController motor_controller_left = AdafruitMotorController(MOTOR_LEFT);
+
+BaseController<AdafruitMotorController, Adafruit_MotorShield> base_controller(nh, &motor_controller_left, &motor_controller_right);
+
+void setup()
+{
+      base_controller.setup();
+    base_controller.init();
+
+    nh.loginfo("Initialize DiffBot Motor Controllers");
+    motor_controller_left.begin();
+    motor_controller_right.begin();
+    nh.loginfo("Setup finished");
+}
+
+void loop()
+{
+      static bool imu_is_initialized;
+
+    base_controller.update();
+
+    if (base_controller.connected() && !imu_is_initialized)
+{
+        imu_is_initialized = base_controller.initIMU();
+}
+
+    if (!base_controller.connected())
+{
+        imu_is_initialized = false;
+}
+
+    nh.spinOnce();
+}
